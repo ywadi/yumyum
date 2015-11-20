75901a1cf509b5905380ebbe065172e6e0338728
ywadi <yousef.wadi@arabiaweather.com>
2015-11-20 06:31:55 +0200

    Add rest of routes

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,18 @@ var Order = Models.Order;
 Order.methods(['get','put','post', 'delete']);
 Order.register(router, "/order");
 
+var Dish = Models.Dish;
+Dish.methods(['get','put','post', 'delete']);
+Dish.register(router, "/dish");
+
+var User = Models.User;
+User.methods(['get','put','post', 'delete']);
+User.register(router, "/dish");
+
+
+//TODO: Handle a token to make sure user is logged in by using middleware 
+//TODO: Upload images route for different types of data 
+//TODO: Draw initial flow and handle it 
 
 
 //Test route for uptime
