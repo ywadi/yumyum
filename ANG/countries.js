2e74ee818adcd645e3bc35401953283c9d0e8352
arabiaweather <yousef.wadi@arabiaweather.com>
2015-12-16 21:50:52 +0200

    Stopped with Countries pages

--- ANG/countries.js
+++ ANG/countries.js
@@ -4,5 +4,12 @@ angular.module('Countries').controller('CountryCTRL',CountryCTRL);
 function CountryCTRL ($scope, $resource, $location)
 {
 	console.log("Running....");
+
+	$scope.getCountries = function(){
+		$scope.countriesList = $resource("http://localhost:3000/admin/v1/:action/",{action:"country"});	
+		$scope.countries = $scope.countriesList.query();
+	};
+	$scope.getCountries();
+
 	
 }
